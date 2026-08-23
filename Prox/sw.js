69779afb5c056e04b8945f3a08780b922fe4f3c1
2d3t <2dv3tr@gmail.com>
2026-08-23 19:17:45 +0300

    Create sw.js

---
+++ Prox/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'hello-pwa-v1';
+const FILES_TO_CACHE = [
+    '/2d3t/Prox/index.html',
+    '/2d3t/Prox/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('[SW] Кэшируем файлы');
+                return cache.addAll(FILES_TO_CACHE);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(name => {
+                    if (name !== CACHE_NAME) {
+                        console.log('[SW] Удаляем старый кэш:', name);
+                        return caches.delete(name);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).catch(() => {
+                    return new Response('Нет интернета', {
+                        status: 503,
+                        statusText: 'Service Unavailable'
+                    });
+                });
+            })
+    );
+});
