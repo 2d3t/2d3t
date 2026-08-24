4dd2eace54f8831fe5d8fdfcd00ea94b2961e8d7
2d3t <2dv3tr@gmail.com>
2026-08-24 09:30:14 +0300

    Update pro/Pro5/sw.js

---
+++ pro/Pro5/sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'klikedit-v1';
+const FILES_TO_CACHE = [
+    '/2d3t/pro/Pro5/index.html',
+    '/2d3t/pro/Pro5/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('[SW] Кэшируем KlikEdit...');
+                return cache.addAll(FILES_TO_CACHE);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        console.log('[SW] Удаляем старый кэш:', name);
+                        return caches.delete(name);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+                return fetch(event.request)
+                    .then((response) => {
+                        if (response && response.status === 200) {
+                            const responseClone = response.clone();
+                            caches.open(CACHE_NAME).then((cache) => {
+                                cache.put(event.request, responseClone);
+                            });
+                        }
+                        return response;
+                    })
+                    .catch(() => {
+                        return caches.match('/2d3t/pro/Pro5/index.html');
+                    });
+            })
+    );
+});
